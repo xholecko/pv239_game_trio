7f6ec1c4d136dc8eead1ad3fc8adcfef86bffcc4
Stefan Holecko <stevoh096@gmail.com>
2019-04-05 04:31:08 +0200

    test1

--- PV239_game_trio/app/src/main/java/com/example/pv239_game_trio/games/tikBum/TikBumActivity.kt
+++ PV239_game_trio/app/src/main/java/com/example/pv239_game_trio/games/tikBum/TikBumActivity.kt
@@ -2,6 +2,7 @@ package com.example.pv239_game_trio.games.tikBum
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.pv239_game_trio.R
 
 class TikBumActivity : AppCompatActivity() {
@@ -12,5 +13,8 @@ class TikBumActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tik_bum)
+
+        Log.d(TAG,"TikBumActivity created")
+
     }
 }
