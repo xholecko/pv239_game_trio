45426f53903ce382bfdbce5875cff25099b159ca
Forewer <47856862+Forewer@users.noreply.github.com>
2019-05-04 14:53:57 +0200

    Oprava zleho commitu

--- PV239_game_trio/app/src/main/java/com/example/pv239_game_trio/games/charade/CharadeActivity.kt
+++ PV239_game_trio/app/src/main/java/com/example/pv239_game_trio/games/charade/CharadeActivity.kt
@@ -11,5 +11,6 @@ class CharadeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_charade)
+        Log.d(TAG,"CharadeActivity created")
     }
 }
