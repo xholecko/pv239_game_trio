e8b5a73729fdd813282950b07054f441d1b689fc
Forewer <47856862+Forewer@users.noreply.github.com>
2019-05-04 14:58:53 +0200

    Oprava zleho commitu

--- PV239_game_trio/app/src/main/java/com/example/pv239_game_trio/games/hangman/HangmanActivity.kt
+++ PV239_game_trio/app/src/main/java/com/example/pv239_game_trio/games/hangman/HangmanActivity.kt
@@ -12,30 +12,14 @@ import com.example.pv239_game_trio.R
 
 
 class HangmanActivity : AppCompatActivity() {
-
-    private lateinit var playBtn:Button
-    private lateinit var spinner: Spinner
-
     private val TAG = "GameTrioHangman"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_hangman_menu)
-        spinner = findViewById<View>(R.id.spinner) as Spinner
-//        spinner.setOnScrollChangeListener{
-//            Log.d(TAG,"button PLAY HANGMAN was pressed")
-//        }
-        playBtn = findViewById<View>(R.id.playBtn) as Button
-        playBtn.setOnClickListener {
-            Log.d(TAG,"button PLAY HANGMAN was pressed")
-            openHangmanGameActivity()
-        }
+        setContentView(R.layout.activity_hangman_menu)    
         Log.d(TAG,"HangmanActivity created")
     }
 
-    private fun openHangmanGameActivity() {
-        val intent = Intent(this, HangmanGameActivity::class.java)
-        startActivity(intent)
-    }
+   
 
 }
